0eda6d139cbd74f1504af2536783f42e0c15f487
John Steve Palero <jtpalero.halcyondigital@gmail.com>
2019-08-23 09:58:29 +0800

    F

--- index.d.ts
+++ index.d.ts
@@ -1,58 +1,57 @@
-declare module "rn-halcyon-cached-image" {
-  import React, { PureComponent } from 'react'
 
-  import * as ReactNative from 'react-native'
+import React, { PureComponent } from 'react'
 
-  type resizeModeType = "contain" | "cover" | "stretch" | "center"
+import * as ReactNative from 'react-native'
 
-  type priorityType = "low" | "normal" | "high"
+type resizeModeType = "contain" | "cover" | "stretch" | "center"
 
-  type uriObjectType = {
-    uri: string
-    /**
-     * Token for caching access.
-     */
-    token: string
-    priority?: priorityType
-    cache?: "immutable" | "web" | "cacheOnly"
-  }
+type priorityType = "low" | "normal" | "high"
 
-  export interface ImageStyle extends ReactNative.FlexStyle, ReactNative.TransformsStyle, ReactNative.ShadowStyleIOS {
-    backfaceVisibility?: 'visible' | 'hidden'
-    borderBottomLeftRadius?: number
-    borderBottomRightRadius?: number
-    backgroundColor?: string
-    borderColor?: string
-    borderWidth?: number
-    borderRadius?: number
-    borderTopLeftRadius?: number
-    borderTopRightRadius?: number
-    overlayColor?: string
-    tintColor?: string
-    opacity?: number
-  }
-
-  export interface CachedImageProps {
-    /**
-     * Can be object, string or asset.
-     * 
-     * if object you need to pass
-     * uri: string,
-     * token: string,
-     * [optional]priority: string,
-     */
-    uri: uriObjectType | string | number;
-    width?: number;
-    height?: number;
-    rounded?: boolean;
-    imageStyle?: ReactNative.StyleProp<ImageStyle>;
-    resize?: resizeModeType;
-  }
-
+type uriObjectType = {
+  uri: string
   /**
-   * CachedImage
-   * 
-   * Image component that caches the images automatically.
+   * Token for caching access.
    */
-  export class CachedImage extends PureComponent<CachedImageProps> { }
+  token: string
+  priority?: priorityType
+  cache?: "immutable" | "web" | "cacheOnly"
 }
+
+export interface ImageStyle extends ReactNative.FlexStyle, ReactNative.TransformsStyle, ReactNative.ShadowStyleIOS {
+  backfaceVisibility?: 'visible' | 'hidden'
+  borderBottomLeftRadius?: number
+  borderBottomRightRadius?: number
+  backgroundColor?: string
+  borderColor?: string
+  borderWidth?: number
+  borderRadius?: number
+  borderTopLeftRadius?: number
+  borderTopRightRadius?: number
+  overlayColor?: string
+  tintColor?: string
+  opacity?: number
+}
+
+export interface CachedImageProps {
+  /**
+   * Can be object, string or asset.
+   * 
+   * if object you need to pass
+   * uri: string,
+   * token: string,
+   * [optional]priority: string,
+   */
+  uri: uriObjectType | string | number;
+  width?: number;
+  height?: number;
+  rounded?: boolean;
+  imageStyle?: ReactNative.StyleProp<ImageStyle>;
+  resize?: resizeModeType;
+}
+
+/**
+ * CachedImage
+ * 
+ * Image component that caches the images automatically.
+ */
+export class CachedImage extends PureComponent<CachedImageProps> { }
